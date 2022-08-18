f261533929171a455fc5ab039610880b119a965e
FILIP <fisk0117@gmail.com>
2022-08-18 21:33:58 +0200

    Steam!

--- projekt1/Assets/Scripts/Shooter.cs
+++ projekt1/Assets/Scripts/Shooter.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Shooter : MonoBehaviour
+public class Shooter : NetworkBehaviour
 {
     public GameObject bullet;
     public Transform shootPos;
@@ -26,6 +27,18 @@ public class Shooter : MonoBehaviour
         }
     }
     void Shoot()
+    {
+        SpawnBulletServer();
+    }
+
+    [Command(requiresAuthority = false)]
+    void SpawnBulletServer()
+    {
+        SpawnBulletClient();
+    }
+
+    [ClientRpc]
+    void SpawnBulletClient()
     {
         GameObject lazerBullet = Instantiate(bullet, shootPos.position, transform.rotation);
         Physics2D.IgnoreCollision(lazerBullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
