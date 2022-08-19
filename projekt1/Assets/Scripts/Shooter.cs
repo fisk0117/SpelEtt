6b4b6855c218890a2045f2d1b4ac96db2c57c59b
tjugosex <mikael.skaremyr.te1@hotmail.com>
2022-08-19 15:29:56 +0200

    lite skjutcd :)

--- projekt1/Assets/Scripts/Shooter.cs
+++ projekt1/Assets/Scripts/Shooter.cs
@@ -10,20 +10,49 @@ public class Shooter : MonoBehaviour
     public float kb = 0.4f;
     public Color color;
     TrailRenderer tr;
+    public float ammo = 3f;
+    public float ammocd = 0f;
+    public float ammocdroof = 3f;
+    public float ammocdcd = 0f;
     void Start()
     {
         tr = bullet.GetComponent<TrailRenderer>();
+        
     }
 
     void Update()
     {
         tr.startColor = color;
         tr.endColor = color;
+        
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && (ammo >= 1) && (ammocd <= 0))
         {
             Shoot();
+            ammo--;
+            Debug.Log(ammo);
         }
+        
+        else if ((ammo <= 0) && (ammocdcd == 0)){
+            ammocd = ammocdroof;
+            ammocdcd = 1;
+
+        }
+
+        if (ammocd >=0)
+        {
+            ammocd -= Time.deltaTime;
+            Debug.Log(ammocd);
+        }
+
+        if ((ammocd <= 0) && (ammocdcd == 1))
+        {
+            ammo = 3f;
+            ammocdcd = 0;
+        }
+
+
+        
     }
     void Shoot()
     {
@@ -33,4 +62,6 @@ public class Shooter : MonoBehaviour
         lazerBullet.GetComponent<Rigidbody2D>().AddForce(shootPos.right * speed);
         gameObject.GetComponent<Rigidbody2D>().AddForce(shootPos.right * -speed * kb);
     }
+
+    
 }
---
+++ projekt1/Assets/Scripts/size.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class size : MonoBehaviour
+{
+    float a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        a = GameObject.FindWithTag("Player").GetComponent<Shooter>().ammocd;
+        Debug.Log(a);
+
+        gameObject.active = true;
+        transform.localScale = new Vector3((1-(a/3)), 0.2f ,1);
+        
+
+       
+       
+    }
+}
