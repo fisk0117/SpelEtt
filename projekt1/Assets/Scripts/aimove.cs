251432a2c5b704014253ad2d13ede3c7a6a9ea38
tjugosex <mikael.skaremyr.te1@hotmail.com>
2022-08-19 16:01:07 +0200

    flib deathmatch

---
+++ projekt1/Assets/Scripts/aimove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aimove : MonoBehaviour
+{
+    Vector3 i;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //i = GameObject.FindGameObjectWithTag("Player").transform.position;
+        // Move our position a step closer to the target.
+        var step =  speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, step);
+
+        // Check if the position of the cube and sphere are approximately equal.
+        if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < 0.001f)
+        {
+            // Swap the position of the cylinder.
+            GameObject.FindGameObjectWithTag("Player").transform.position *= -1.0f;
+        }
+    }
+}
