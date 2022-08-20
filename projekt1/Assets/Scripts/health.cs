99f2f94669f4f424b2bb06fa84a77bcc4d216cac
tjugosex <mikael.skaremyr.te1@hotmail.com>
2022-08-20 17:19:39 +0200

    hp

---
+++ projekt1/Assets/Scripts/health.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    public float hp;
+    public float starthp = 3;
+    bool death = false;
+    public GameObject livingplayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = starthp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            death = true;
+        }
+
+        if (death)
+        {
+               livingplayer.transform.position = new Vector3(0, 0, 0);
+               hp = starthp;  
+               death = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "bullet")
+        {
+            Destroy(other.gameObject);
+            hp--;
+        }
+    }
+}
